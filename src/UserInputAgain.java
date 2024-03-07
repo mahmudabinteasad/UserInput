998d8b4c1fd82f14c542bf5f5044452cdaef43fe
samiaasad <samiaasad604@gmail.com>
2024-03-07 21:00:16 +0600

    Get user input

---
+++ src/UserInputAgain.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class UserInputAgain{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter an integer value: ");
+        int i = sc.nextInt();
+        System.out.println("You have entered " + i);
+    }
+}
